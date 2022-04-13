e58a812635b5e73bab6975f856f01e8148f3d5ac
Jesse Iberri <jesseiberri@gmail.com>
2022-04-13 15:44:07 -0700

    Added settings popup menu

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 "use strict";
 
+class Settings {
+    constructor() {
+        this.menu = document.getElementById('menuContent');
+        this.guesses = document.getElementById('menu-guesses');
+        this.startRange = document.getElementById('menu-startRange');
+        this.endRange = document.getElementById('menu-endRange');
+
+        this.inputList = [this.guesses, this.startRange, this.endRange];
+    }
+}
+
 class Progress {
     constructor() {
         this.table = document.getElementById('progress');
@@ -35,25 +46,37 @@ class Result {
 
 let content = document.getElementById('content');
 let guessField = document.getElementById('guessField');
-let result = document.getElementById('resultParas');
 let submitButton = document.getElementById('submit-button');
 let submitButtonDisplay = submitButton.style.display;
 let resultsTable = document.getElementById('results');
 let resultsTableDisplay = resultsTable.style.display;
+let menuButton = document.getElementById('menuButton');
+let menuExitButton = document.getElementById('menuExit');
+let menuOp = document.getElementById('menuOp');
+let saveChangesButton = document.getElementById('saveChanges');
+let discardChangesButton = document.getElementById('discardChanges');
 let progress = new Progress();
+let settings = new Settings();
 
 let results = [];
 let currentResult = null;
 
-let randomNumber = () => parseInt(Math.random() * (101 - 1) + 1);
+let numGuesses = 10;
+let startRange = 1;
+let endRange = 100;
+
+let randomNumber = () => parseInt(Math.random() * (endRange+1 - startRange) + startRange);
 let answer;
 let turns;
 let gameOver;
+
+let settingsChanged = false;
 
 submitButton.addEventListener('click', () => {
     clearResponse();
     newGuess();
 });
+
 guessField.addEventListener('input', clearResponse);
 guessField.addEventListener('keyup', (ev) => {
     if (ev.key === "Enter") {
@@ -61,13 +84,93 @@ guessField.addEventListener('keyup', (ev) => {
     }
 });
 
+menuButton.addEventListener('click', () => {
+    loadSettings();
+    menuOp.classList.add("show-95");
+});
+
+let hideMenu = () => {
+    menuOp.classList.remove('show-95');
+}
+
+for (let i = 0; i < settings.inputList.length; i++) {
+    let inputSetting = settings.inputList[i];
+    inputSetting.addEventListener('input', () => {
+        settingsChanged = true;
+    });
+}
+
+menuExitButton.addEventListener('click', hideMenu);
+
+saveChangesButton.addEventListener('click', () => {
+    let startNewGame = false;
+    if (settingsChanged) {
+        let newGuess = parseInt(settings.guesses.value);
+        let newStartRange = parseInt(settings.startRange.value);
+        let newEndRange = parseInt(settings.endRange.value);
+        let getLabel = (e) => e.labels[0].innerHTML.replace(":", "");
+
+        let checkNum = (val, el) => {
+            if (isNaN(val)) {
+                window.alert(`${getLabel(el)} must be a number.`);
+                return false;
+            }
+
+            return true;
+        };
+
+        if (!checkNum(newGuess, settings.guesses) ||
+            !checkNum(newStartRange, settings.startRange) ||
+            !checkNum(newEndRange, settings.endRange)) return;
+
+        if (newGuess === 0) {
+            window.alert('Number of guesses can not be 0.');
+            return;
+        }
+
+        if (newStartRange > newEndRange) {
+            window.alert('Start range can not be greater than end range.');
+            return;
+        }
+
+        if (newStartRange < 0 || newEndRange < 0) {
+            window.alert('Range must be positive');
+            return;
+        }
+
+        if (settingsChanged && !confirm("Changing settings will reset the game, are you sure you want to continue?")) {
+            return;
+        }
+
+        numGuesses = newGuess;
+        startRange = newStartRange;
+        endRange = newEndRange;
+
+        window.alert('Settings successfully saved.');
+        startNewGame = true;
+    }
+
+    settingsChanged = false;
+    hideMenu();
+
+    if (startNewGame) newGame();
+});
+
+discardChangesButton.addEventListener('click', () => {
+    if(settingsChanged && !confirm("You have unsaved modifications to settings, are you sure you want to discard changes?")) {
+        return;
+    }
+    settingsChanged = false;
+    hideMenu();
+});
+
 newGame();
 
 function newGame() {
     answer = randomNumber();
-    turns = 10;
+    turns = numGuesses;
 
-    progress.guesses.innerHTML = turns;
+    progress.guesses.innerHTML = numGuesses;
     progress.lastResult.innerHTML = "";
     progress.lowOrHi.innerHTML = "";
 
@@ -95,8 +198,8 @@ function newGuess () {
         return;
     }
 
-    if (value < 1 || value > 100) {
-        addResponse('Please enter a number between 1 and 100.', true);
+    if (value < startRange || value > endRange) {
+        addResponse(`Please enter a number between ${startRange} and ${endRange}.`, true);
         return;
     }
 
@@ -189,4 +292,10 @@ function clearResponse () {
         let element = responses[r];
         element.remove();
     }
+}
+
+function loadSettings () {
+    settings.guesses.value = numGuesses;
+    settings.startRange.value = startRange;
+    settings.endRange.value = endRange;
 }
